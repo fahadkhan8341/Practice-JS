74ce42b667665228b26fe5c034f575dbfdaea530
fahad8341 <fahadkhan8341822644@gmail.com>
2020-08-20 22:28:27 +0530

    modified typing master game

--- Traversy Media Projects/Typing Game/app.js
+++ Traversy Media Projects/Typing Game/app.js
@@ -1,32 +1,35 @@
 const words = ["copper", "pop", "gregarious", "aquarium", "stand", "hell", "shareholder", "brick", "outline", "border", "deviation", "wrestle", "moment", "celebration", "landscape", "suggest", "dump", "relationship", "reason", "cottage", "opinion", "copyright", "rank", "creation", "ecstasy", "advantage", "specimen", "frank", "bread", "marsh", "curl", "complex", "choose", "adviser", "promotion", "canvas", "hunting", "declaration", "oil", "collar", "burial", "club", "dialect", "treatment", "welfare", "reject", "knee", "origin", "theft", "era"];
 
+const container = document.querySelector('.container')
 const currentWord = document.querySelector('.currentText')
 const inputField = document.querySelector('.input')
 const scoreContainer = document.querySelector('.score')
-const timeLeftContainer=document.querySelector('.timeLeft')
-const highScoreContainer=document.querySelector('.highScore')
-const match=document.querySelector('.match')
+const timeLeftContainer = document.querySelector('.timeLeft')
+const highScoreContainer = document.querySelector('.highScore')
+const match = document.querySelector('.match')
 let score = 0;
-let timeLeft=5;
+let timeLeft = 5;
 let lastIndex;
 let timer;
-let highScore=0;
+let highScore = 0;
 
 window.addEventListener('load', init)
 inputField.addEventListener('keyup', startGame)
+window.addEventListener('load', () => {
+    highScore = localStorage.getItem('highScore')
+    highScoreContainer.innerHTML = highScore
+})
 
-highScore = localStorage.getItem('highScore')
-highScoreContainer.innerHTML=highScore
 
 function init() {
     inputField.value = ''
-    let wordIndex= random()
-    if(wordIndex==lastIndex){
-        wordIndex=random()
+    let wordIndex = random()
+    if (wordIndex == lastIndex) {
+        wordIndex = random()
     }
     currentWord.innerHTML = words[wordIndex]
-    lastIndex=wordIndex
-   
+    lastIndex = wordIndex
+
 }
 
 function random() {
@@ -37,37 +40,37 @@ function startGame(e) {
     if (e.keyCode === 13) {
         if (inputField.value === currentWord.innerHTML) {
             clearInterval(timer)
-            timeLeft=5
+            timeLeft = 5
+            container.style.animation = '';
             startTimer()
+            timer = setInterval(startTimer, 1000)
             init()
             updateScore()
             inputField.value = ''
-            match.innerHTML=showMatch('success','Correct Match')
-        } else{
-            match.innerHTML=showMatch('warning','Wrong Match')
+            match.innerHTML = showMatch('success', 'Correct Match')
+        } else {
+            match.innerHTML = showMatch('warning', 'Wrong Match')
         }
     }
 }
 
 function startTimer() {
-   timer = setInterval(() => {
+    if (timeLeft == 0) {
 
-        if(timeLeft==-1){
-            timeLeft=5
-            clearInterval(timer)
-            
-            if(score>highScore){
-                highScore=score;
-                highScoreContainer.innerHTML=highScore
-                localStorage.setItem('highScore',highScore)
-            }
-            score=0;
-            match.innerHTML=showMatch('danger','Game Over')
+        clearInterval(timer)
 
+        if (score > highScore) {
+            highScore = score;
+            highScoreContainer.innerHTML = highScore
+            localStorage.setItem('highScore', highScore)
         }
-        timeLeftContainer.innerHTML=timeLeft;
-        timeLeft--;
-    }, 1000);
+        score = 0;
+        match.innerHTML = showMatch('danger', 'Game Over')
+        container.style.animation = "shake .7s forwards ease-out"
+    }
+    timeLeftContainer.innerHTML = timeLeft;
+    timeLeft--;
+
 }
 
 function updateScore() {
@@ -75,6 +78,6 @@ function updateScore() {
     scoreContainer.innerHTML = score;
 }
 
-function showMatch(color,match){
+function showMatch(color, match) {
     return `<i class="text-${color}">${match}</i>`
 }
